cd116a80828c0ad3c215d146107d075a140f1697
Silakova <ir.silackowa@yandex.ru>
2024-07-20 18:16:15 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,22 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        // Объявляете переменные для входных данных и
+        // параметров программы: одну для хранения
+        // стоимости билета, другую для хранения количества
+        // рублей для одной бонусной мили
+
+        // Рассчитываете количество бонусных миль, используя
+        // значения заведённых переменных. Ответ сохраняете в
+        // новую переменную и выводите на экран
+
+        double amount = 12_499.99;
+        int rublesPerMile = 20;
+
+        var miles = (int) amount / rublesPerMile;
+
+        System.out.println(miles);
+    }
+}
